41be53a3470e05aa567b56f27dae8b25715167da
BIG BOI <patrickmonto@gmail.com>
2018-10-23 14:25:35 +0100

    added HTML, CSS Elements using js.

--- js/script.js
+++ js/script.js
@@ -1,18 +1,23 @@
 
 $('select').on('change', function() {
-    
+$('.selection-container').addClass('selection');
+$('.api-loader').empty();
+
     console.log('test');
     var url = "https://api.nytimes.com/svc/topstories/v2/" + $('select').val() + ".json";
 url += '?' + $.param({
   'api-key': "82f12f3e4189492f86578cb2d4e6add0"
 });
+
 $.ajax({
   url: url,
   method: 'GET',
 }).done(function(result) {
-    $('api-loader').append('<p> '+ result.results[0].abstract +' </p>')
+  for(var i=0; i<12; i++) {
+    $('.api-loader').append('<a href="'+result.results[i].url+'"><div class="news-item"><p> '+ result.results[i].abstract +' </p></div></a>').css('color', 'white');
+    $('.news-item').last().css('background-image', 'url("'+result.results[i].multimedia[4].url+'")');
+  }
   console.log(url);
-  alert(url);
 }).fail(function(err) {
   throw err;
 });
