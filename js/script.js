06210e6bae459dc3f806e2d04e4b07aebbcfc0f8
BIG BOI <patrickmonto@gmail.com>
2018-10-24 18:19:03 +0100

    added animations

--- js/script.js
+++ js/script.js
@@ -1,9 +1,6 @@
-
-$('select').on('change', function() {
-$('.flex-container').addClass('select');
-$('.api-loader').empty();
-
-    console.log('test');
+$('.dropdown').on('change', function(event){
+  $('.api-loader').empty();
+  $('.flex-container').addClass('select');
     var url = "https://api.nytimes.com/svc/topstories/v2/" + $('select').val() + ".json";
 url += '?' + $.param({
   'api-key': "82f12f3e4189492f86578cb2d4e6add0"
@@ -13,12 +10,19 @@ $.ajax({
   url: url,
   method: 'GET',
 }).done(function(result) {
-  for(var i=0; i<12; i++) {
+  var newsCount = 0;
+  for(var i=0; i<result.results.length; i++) {
+    if (result.results[i].multimedia.length < 4) {
+      continue;}
     $('.api-loader').append('<a href="'+result.results[i].url+'"><div class="news-item"><p> '+ result.results[i].abstract +' </p></div></a>').css('color', 'white');
     $('.news-item').last().css('background-image', 'url("'+result.results[i].multimedia[4].url+'")');
+ newsCount++;
+ if (newsCount>=12){
+   break;
+ }
   }
-  console.log(url);
 }).fail(function(err) {
   throw err;
 });
+
 });
